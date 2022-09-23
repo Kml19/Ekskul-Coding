ba777ca0e6fdf8f64372513e7c77644802d8d366
Kamil <75714850+Kml19@users.noreply.github.com>
2022-09-23 17:12:58 +0700

    Commit Pertama
    Tugas Sederhana Mencari Luas Bangun persegi panjang

---
+++ Program Sederhana Mencari Luas Bangun.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	int luas, panjang, lebar;
+	
+	cout<<"=======================================\n";
+	cout<<"Program Menghitung Luas Persegi Panjang\n";
+	cout<<"=======================================\n";
+	
+	cout<<"Masukkan Panjang ="; cin>>panjang;
+	cout<<"Masukkan Lebar ="; cin>>lebar;
+	
+	luas = panjang * lebar;
+	
+	cout<<"Luas               = Panjang x Lebar\n";
+	cout<<"                   = "<<panjang<<" x "<<lebar<<"\n";
+	cout<<"                   = "<<lebar<<"\n";
+	
+	
+	return 0;
+	
+}
